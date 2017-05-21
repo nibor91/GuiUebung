b310a31cb9e189dbc4c41fff168bbddb222a7803
robin <robin.kernel@hotmail.com>
2017-05-21 18:56:47 +0200

    Fenster nicht fertig weil mathias mich gehätzt hat

--- src/com/sabel/Fenster.java
+++ src/com/sabel/Fenster.java
@@ -6,4 +6,24 @@ import javax.swing.*;
  * Created by robin on 21.05.17.
  */
 public class Fenster extends JFrame {
+    private MyTableModel myTableModel;
+    private JTable jTable;
+
+    public Fenster() {
+        initComponents();
+        initEvents();
+        buildWindow();
+    }
+
+    private void initComponents() {
+
+    }
+
+    private void buildWindow() {
+
+    }
+
+    private void initEvents() {
+
+    }
 }
